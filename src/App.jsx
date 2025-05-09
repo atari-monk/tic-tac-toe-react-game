dd795fd3b68dfaeaffb83c15d667e598e3a0d77a
atari-monk <atari.monk1@gmail.com>
2025-05-09 11:35:52 +0200

    Raw Game

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './tic-tac-toe/Square.css'
+import './tic-tac-toe/Board.css'
+import './tic-tac-toe/Game.css'
+import Game from './tic-tac-toe/Game.jsx'
+
+function App() {
+  return (
+    <>
+      <Game></Game>
+    </>
+  )
+}
+
+export default App

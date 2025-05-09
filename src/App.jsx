4daf856f3c4d9dfc9f6f655ccf304b3c7896f99e
atari-monk <atari.monk1@gmail.com>
2025-05-09 12:36:09 +0200

    Scalable grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import './App.css'
 import './tic-tac-toe/Square.css'
 import './tic-tac-toe/Board.css'
-import './tic-tac-toe/Game.css'
 import Game from './tic-tac-toe/Game.jsx'
 
 function App() {
--- src/tic-tac-toe/Square.jsx
+++ src/tic-tac-toe/Square.jsx
@@ -1,7 +1,31 @@
+import {useRef, useEffect, useState} from 'react'
+
 export default function Square({value, onSquareClick}) {
+  const ref = useRef(null)
+  const [fontSize, setFontSize] = useState('1rem')
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(([entry]) => {
+      const {width, height} = entry.contentRect
+      const size = Math.min(width, height)
+      setFontSize(`${size * 0.6}px`) // 60% of cell size
+    })
+
+    if (ref.current) {
+      resizeObserver.observe(ref.current)
+    }
+
+    return () => resizeObserver.disconnect()
+  }, [])
+
   return (
-    <button className="square" onClick={onSquareClick}>
+    <div
+      className="square"
+      ref={ref}
+      onClick={onSquareClick}
+      style={{fontSize}}
+    >
       {value}
-    </button>
+    </div>
   )
 }
